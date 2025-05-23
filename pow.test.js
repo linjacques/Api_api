d81afc005c1f4c3915c59996a376cfe26f776b82
linjacques <linjacques463@gmail.com>
2025-05-23 15:18:15 +0200

    fix3

--- pow.test.js
+++ pow.test.js
@@ -2,6 +2,6 @@ import { Pow } from "./pow";
 
 describe("teste des fonctions Puissance", () => {
     test("2 puissance 4",() => {
-        expect(Pow(2, 2), toBe(4));
+        expect(Pow(2, 2).toBe(4));
     })
 })
